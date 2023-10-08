3dba3a732291b3cabb701dafaf4f7a091eceb1e1
Adarsh-S1 <adarshvillasuni@gmail.com>
2023-10-08 09:30:22 +0530

    404 page added

--- app.js
+++ app.js
@@ -30,12 +30,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
 app.use((req, res, next) => {
   res.status(404).render("404");
 }); 
   
-app.use('/', indexRouter);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
